024c1d039f487b7998df3ee64f7c789e520ccdae
LordCracken <vladislav070201@gmail.com>
2023-03-30 17:18:13 +0300

    use state for managing name and amount fields in ingredients form

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,20 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = () => {
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const nameChangeHandler = event => {
+    setName(event.target.value);
+  };
+
+  const amountChangeHandler = event => {
+    setAmount(event.target.value);
+  };
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +26,11 @@ const IngredientForm = () => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={name} onChange={nameChangeHandler} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={amount} onChange={amountChangeHandler} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
