66a4044d7857a347405eb5ac90f81ca283630b44
LordCracken <vladislav070201@gmail.com>
2023-03-30 21:25:32 +0300

    prevent unnecessary requests to server on user search input

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -8,24 +8,37 @@ const url =
 
 const Search = ({ onLoadIngredients }) => {
   const [filter, setFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = filter.length ? `?orderBy="title"&equalTo="${filter}"` : '';
+    const timer = setTimeout(() => {
+      if (filter === inputRef.current.value) {
+        const query = filter.length ? `?orderBy="title"&equalTo="${filter}"` : '';
 
-    const getIngredients = async () => {
-      const response = await fetch(url + query);
-      const resData = await response.json();
-      const loadedIngredients = [];
+        const getIngredients = async () => {
+          const response = await fetch(url + query);
+          const resData = await response.json();
+          const loadedIngredients = [];
 
-      for (const key in resData) {
-        loadedIngredients.push({ id: key, title: resData[key].title, amount: resData[key].amount });
+          for (const key in resData) {
+            loadedIngredients.push({
+              id: key,
+              title: resData[key].title,
+              amount: resData[key].amount,
+            });
+          }
+
+          onLoadIngredients(loadedIngredients);
+        };
+
+        getIngredients();
       }
+    }, 500);
 
-      onLoadIngredients(loadedIngredients);
+    return () => {
+      clearTimeout(timer);
     };
-
-    getIngredients();
-  }, [filter, onLoadIngredients]);
+  }, [filter, onLoadIngredients, inputRef]);
 
   const filterChangeHandler = event => {
     setFilter(event.target.value);
@@ -36,7 +49,7 @@ const Search = ({ onLoadIngredients }) => {
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={filter} onChange={filterChangeHandler} />
+          <input ref={inputRef} type="text" value={filter} onChange={filterChangeHandler} />
         </div>
       </Card>
     </section>
